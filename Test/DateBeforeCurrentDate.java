9b9f995817db82cc58f78e8d815458fd36c84ecd
Akash Chawla <akash.chawla@ymail.com>
2016-06-19 19:23:23 -0400

    User stories Implemented Akash- Sprint1
    User stories US01 and US22 Implemented by Akash Chawla (ac) for Sprint 1
    and Read file name from Command line argument

---
+++ Test/DateBeforeCurrentDate.java
@@ -0,0 +1,24 @@
+package Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateBeforeCurrentDate {
+    public static boolean compare(String datesString) throws ParseException {
+        SimpleDateFormat format = new SimpleDateFormat("dd MMM yyyy");
+
+        Date dates = format.parse(datesString);
+
+        Date currentDate = new Date();
+        
+        
+
+        if (dates.compareTo(currentDate) == 1) {
+            System.out.println("Error: All dates should be less than current dates");
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
--- readFile.java
+++ readFile.java
@@ -2,12 +2,22 @@ import Model.Family;
 import Model.Individual;
 import Processor.ParseData;
 import java.lang.Exception;
-
 import Test.*;
 
 public class readfile {
+
 	public static void main(String[] args) {
-		String filename = "My-Family-21-May-2016.ged";
+		String filename = null;
+		try { 
+	    	// The name of the file to open.
+	    	   filename = args[0];    	   
+	       }
+	       
+	       catch (ArrayIndexOutOfBoundsException e){
+	     	  System.out.println("File name not provided as argument");
+	     	  System.exit(1);
+	     	 }
+	       
 		ParseData p = new ParseData();
 		try {
 			p.readFile(filename);
@@ -23,8 +33,14 @@ public class readfile {
 					System.out.println("Death Date: " + ind.getDeathDate());
 				}
 				
+				Test.UniqueID.checkUniqueId(ind, p.individuals);
+
 				if (ind.getDeathDate() != null) {
 					Test.BirthBeforeDeath.compare(ind.getBirthDate(), ind.getDeathDate());
+				}
+				
+				if (ind.getBirthDate() != null) {
+					Test.DateBeforeCurrentDate.compare(ind.getBirthDate());
 				}
 				
 				System.out.println();
@@ -38,17 +54,27 @@ public class readfile {
 				System.out.println("Family HUSBAND: " + fam.getHusband().getName());
 				System.out.println("Family Wife: " + fam.getWife().getName());
 				System.out.println("Family Wedding Date: " + fam.getWeddingDate());
-
+				System.out.println("Family Divorce Date: " + fam.getDivorceDate());
+				
 				if (fam.getHusband() != null && fam.getWeddingDate() != null)
 					Test.BirthBeforeMarriage.compare(fam.getHusband().getBirthDate(), fam.getWeddingDate());
 
 				if (fam.getWife() != null && fam.getWeddingDate() != null)
 					Test.BirthBeforeMarriage.compare(fam.getWife().getBirthDate(), fam.getWeddingDate());
+				
+				if (fam.getWeddingDate() != null) {
+					Test.DateBeforeCurrentDate.compare(fam.getWeddingDate());
+				}
+				
+				if (fam.getDivorceDate() != null) {
+					Test.DateBeforeCurrentDate.compare(fam.getDivorceDate());
+				}
 
 			}
 			System.out.println();
 			
 		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 }
