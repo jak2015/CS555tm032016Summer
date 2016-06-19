0060c9ae911ccdaf22025ebd985c144c00e1ea9b
jak2015 <jayashri.kanse@gmail.com>
2016-06-19 17:13:50 -0400

    User stories Implemented Jayshree - Sprint1

--- readFile.java
+++ readFile.java
@@ -1,70 +1,54 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Arrays;
+import Model.Family;
+import Model.Individual;
+import Processor.ParseData;
+import java.lang.Exception;
 
-public class readFile {
+import Test.*;
+
+public class readfile {
 	public static void main(String[] args) {
 		String filename = "My-Family-21-May-2016.ged";
-		String line = null;		
-		
+		ParseData p = new ParseData();
 		try {
-			FileReader reader = new FileReader(filename);
-			BufferedReader br = new BufferedReader(reader);
-			
-			if((line = br.readLine()) != null) {
-				System.out.println(line);
+			p.readFile(filename);
+			System.out.println("=== Individuals Information ===");
+			for (int i = 0; i < p.individuals.size(); i++) {
+				Individual ind = p.individuals.get(i);
+				System.out.println("ID: " + ind.getId());
+				System.out.println("Given Name: " + ind.getGivenName());
+				System.out.println("Family Name: " + ind.getSurName());
+				System.out.println("Sex: " + ind.getSex());
+				System.out.println("Birth Date: " + ind.getBirthDate());
+				if (ind.getDeathDate() != null) {
+					System.out.println("Death Date: " + ind.getDeathDate());
+				}
+				
+				if (ind.getDeathDate() != null) {
+					Test.BirthBeforeDeath.compare(ind.getBirthDate(), ind.getDeathDate());
+				}
+				
+				System.out.println();
 			}
 			
-			while ((line = br.readLine()) != null) {
+			System.out.println("=== Families Information ===");
+			for (int i = 0; i < p.families.size(); i++) {
 				System.out.println("");
-				
-				// line1
-				System.out.println(line);
-				
-				// Splitting the line into words
-				String[] words = line.split(" ");
-				
-				//line 2 should print level
-				System.out.println("Level is " + words[0]); 
-				
-				//line 3 should print tag
-				System.out.print("Tag is ");
-				if(Integer.parseInt(words[0]) == 0) {
-					if(isValidTag(words[1]) == false) {
-						if(isValidTag(words[2]) == false) {
-							System.out.println("Invalid");	
-						}
-						else {
-							System.out.println(words[2]);
-						}	
-					}
-					else {
-						System.out.println(words[1]);
-					}	
-				}
-				else {
-					System.out.println( (isValidTag(words[1]) ? words[1] : "Invalid") );
-				}
-			}
-			br.close();
-		} 
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static boolean isValidTag(String tagName) {
-		
-		String validTags[] = {"SEX", "BIRT", "DEAT", "INDI","NAME","FAMC","FAMS","FAM","MARR","HUSB","WIFE","CHIL","DIV","DATE","HEAD","TRLR","NOTE"};
-		
-		if(Arrays.asList(validTags).contains(tagName)) {
-			return true;
-		}
-		else {
-			return false;
-		}
-		
-	}
+				Family fam = p.families.get(i);
+				System.out.println("Family ID: " + fam.getId());
+				System.out.println("Family HUSBAND: " + fam.getHusband().getName());
+				System.out.println("Family Wife: " + fam.getWife().getName());
+				System.out.println("Family Wedding Date: " + fam.getWeddingDate());
 
+				if (fam.getHusband() != null && fam.getWeddingDate() != null)
+					Test.BirthBeforeMarriage.compare(fam.getHusband().getBirthDate(), fam.getWeddingDate());
+
+				if (fam.getWife() != null && fam.getWeddingDate() != null)
+					Test.BirthBeforeMarriage.compare(fam.getWife().getBirthDate(), fam.getWeddingDate());
+
+			}
+			System.out.println();
+			
+		} catch (Exception e) {
+		}
+	}
 }
