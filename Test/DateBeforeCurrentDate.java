aeb11a683f0a033a2d8cb1b20588a034af4ee9e9
Akash Chawla <akash.chawla@ymail.com>
2016-07-16 13:44:34 -0400

    Changes for Feedback : AC
    Changes for Feedback (by Prof Rowland) implemented by AC

--- Test/DateBeforeCurrentDate.java
+++ Test/DateBeforeCurrentDate.java
@@ -5,7 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DateBeforeCurrentDate {
-    public static boolean compare(String datesString) throws ParseException {
+    public static boolean compare(String datesString, String id, String event, String entityType) throws ParseException {
         SimpleDateFormat format = new SimpleDateFormat("dd MMM yyyy");
 
         Date dates = format.parse(datesString);
@@ -15,7 +15,7 @@ public class DateBeforeCurrentDate {
         
 
         if (dates.compareTo(currentDate) == 1) {
-            System.out.println("Error: All dates should be less than current dates");
+            System.out.println("Error: " +event+ " date should be less than current date for " +entityType+ " : " +id);
             return false;
         } else {
             return true;
